1b994f42f2e821c2a6a2910e1e92751406e8853c
Amir Babaei <babaei.a94@gmail.com>
2016-01-04 17:58:30 -0800

    first update
    testing github

---
+++ app/src/main/java/com/example/amir/funfacts/FunFactsActivity.java
@@ -0,0 +1,43 @@
+package com.example.amir.funfacts;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class FunFactsActivity extends AppCompatActivity {
+    // declare vars
+    private FactBook mFactBook = new FactBook();
+    private ColorWheel mColorWheel = new ColorWheel();
+    private TextView mFactTexVew;
+    private Button mShowFactButton;
+    private RelativeLayout mRelativeLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fun_facts);
+
+        mFactTexVew = (TextView) findViewById(R.id.FactTextView);
+        mShowFactButton = (Button) findViewById(R.id.ShowFactButton);
+        mRelativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fact = mFactBook.getfacts();
+                int color = mColorWheel.getColors();
+                mFactTexVew.setText(fact);
+                mRelativeLayout.setBackgroundColor(color);
+                mShowFactButton.setTextColor(color);
+            }
+        };
+        mShowFactButton.setOnClickListener(listener);
+
+       // Toast.makeText(FunFactsActivity.this, "It works bro!!!", Toast.LENGTH_SHORT).show();
+    }
+}
